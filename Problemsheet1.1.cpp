b55a6bc3e1c6c5f2220e4a800f3760bbd4b160dc
Debarghya Mondal <debarghyamondal15@gmail.com>
2021-08-11 18:13:05 +0530

    Update Problemsheet1.1.cpp
    Corrected code, with comments.

--- Problemsheet1.1.cpp
+++ Problemsheet1.1.cpp
@@ -1,73 +1,58 @@
 //Debarghya Mondal
-// debarghya.mondal2020@vitstudent.ac.in
-// 20BCE0690
+//20BCE0690
+//debarghya.mondal2020@vitstudent.ac.in
 
-#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    long int n;//input number of players
-    cin>>n;
-
-    //input array
-    long int arr[n];
-    for(int i=0;i<n;i++)
-    {
-        cin>>arr[i];
+int main(){
+    int n,max,max2,min,min2;
+//input the number of players
+    cin >> n;
+    int arr[n];
+//loop to input the score of each player
+    for (int i = 0; i <n ; ++i) {
+        cin >> arr[i];
     }
-
-    //Finding winner
-    int winner =0;
-    for(int i=0;i<n;i++)
-    {
-        if(arr[winner]<arr[i])
-        {
-            winner=i;
+    max = -1;
+    max2 = -1;
+ //loop to find max and second max score:
+    for (int i = 0; i <n ; ++i) {
+        if (arr[i]>max){
+            max2 = max;
+            max = arr[i];
+        }
+        else if (arr[i]>max2 && arr[i]<max){
+            max2 = arr[i];
         }
     }
-    cout<<"Winner is Player: "<<winner+1<<endl;
-
-     //Finding next winner
-    long int next_winner=-1;
-    for(int i=0;i<n;i++)
-    {
-        if (arr[i] != arr[winner]) {
-            // first change the value of second largest
-            // as soon as the next element is found
-            if (next_winner == -1)
-                next_winner = i;
-            else if (arr[i] > arr[next_winner])
-                next_winner = i;
+ //loop to find the min and second min score:
+    min = INT_MAX;
+    min2 = INT_MAX;
+    for (int i = 0; i < n; ++i) {
+        if (arr[i]<min){
+            min2 = min;
+            min = arr[i];
+        }
+        else if (arr[i]<min2 && arr[i]>min){
+            min2 = arr[i];
         }
     }
-    cout<<"Next Winner is Player: "<<next_winner+1<<endl;
-
-
-    // Finding worst player:
-    long int worst=0;
-    for (int i=0;i<n;i++)
-    {
-        if(arr[worst]>arr[i])
-        {
-            worst=i;
+//loop to print the result:
+    for (int i = 0; i <n ; ++i) {
+        if (arr[i] == max){
+            cout <<"The winner is: " << i+1 <<" th player."<<endl;
+        }
+        else if (arr[i] == max2){
+            cout <<"The second winner is: " <<i+1 <<" th player."<<endl;
+        }
+        else if (arr[i] == min){
+            cout <<"The worst player is: " << i+1 <<" th player"<<endl;
+        }
+        else if (arr[i]==min2){
+            cout <<"The next worst player is: "<<i+1 <<" th player"<<endl;
         }
     }
-    cout<<"Worst Player is: "<<worst+1<<endl;
 
-    //FInding next worst player
-    long int next_worst=-1;
-    for(int i=0;i<n;i++)
-    {
-        if(arr[i] != arr[worst]){
-            if(next_worst==-1)
-                next_worst=i;
-            else if(arr[i]<arr[next_worst])
-                next_worst=i;
-        }
-    }
-    cout<<"Next Worst Player is: "<<next_worst+1<<endl;
-
-    return 0;
+return 0;
 }
