88957c8fe3eb656fa64c0dec609af9fff6e9c898
Debarghya Mondal <debarghyamondal15@gmail.com>
2021-08-17 12:19:56 +0530

    Add files via upload
    Iterative method for finding maximum and minimum in an array.
    T(n) = O(n)

---
+++ maxminiter.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+void MaxMiniterative(int arr[],int n){
+    int a[2];
+    int max = -1,min = INFINITY;
+    for (int i = 0; i <n ; ++i) {
+        if (arr[i] > max){
+            max = arr[i];
+        }
+    }
+    a[0] = max;
+    for (int i = 0; i <n ; ++i) {
+        if (arr[i]<min){
+            min = arr[i];
+        }
+    }
+    a[1] = min;
+    cout << a[0] <<" "<<a[1] <<endl;
+}
+
